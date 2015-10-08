91fc7740ab329236fe85981fa980f56c7e988787
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-10-08 12:01:00 -0500

    Addons

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
 
+    $('.btn-delete').click(function(){
+
+        alertify.confirm("¿Seguro?",
+            function(e){
+                if(e){
+                    alertify.success('Listo');
+                }else{
+                    alertify.error('Cancelado');
+                }
+            });
+
+        return false;
+    });
+
+    $( 'textarea.ckeditor').each( function() {
+        CKEDITOR.replace( $(this).attr('id') );
+
+    });
 
 });
