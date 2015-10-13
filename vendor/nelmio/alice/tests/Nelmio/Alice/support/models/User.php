b7d46e74676c5e4f90b46fa1baa4e698940ae2da
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-10-12 22:27:35 -0500

    Crafed

---
+++ vendor/nelmio/alice/tests/Nelmio/Alice/support/models/User.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Nelmio\Alice\support\models;
+
+class User
+{
+    public $uuid;
+    public $username;
+    public $fullname;
+    public $birthDate;
+    public $email;
+    public $favoriteNumber;
+    public $friends;
+
+    public function __construct($username = null, $email = null, \DateTime $birthDate = null)
+    {
+        $this->username = $username ?: 'tmp-username';
+        $this->email = $email;
+        $this->birthDate = $birthDate;
+    }
+
+    public static function create($username = null, $email = null, \DateTime $birthDate = null)
+    {
+        return new static($username . '-from-create', $email, $birthDate);
+    }
+
+    public static function bogusCreate($username = null, $email = null, \DateTime $birthDate = null)
+    {
+    }
+
+    public function getAge()
+    {
+        return 25;
+    }
+
+    public function doStuff($dummy, $dummy2, $arg)
+    {
+        $this->username = $arg;
+    }
+
+    public function customSetter($key, $value)
+    {
+        $this->$key = $value . ' set by custom setter';
+    }
+}
