9348d073ad524b36942fa5adaefde0ff4da8e915
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-12-14 09:13:01 -0500

    CollectionsJS

--- Resources/public/js/app.js
+++ Resources/public/js/app.js
@@ -7,8 +7,50 @@ $(document).ready(function(){
         if (r != true) {
             event.preventDefault();
         }
+    });
+
+    $('.add-collection').on('click', function(){
+
+        divParent = $(this).parent();
+        formParent = divParent.parent();
+
+        ordenesColeccion = formParent.find($('table tbody.collections'));
+
+        ordenesColeccion.data('index', ordenesColeccion.find(':input').length);
+
+        addOrdenForm(ordenesColeccion);
+    });
+
+    function addOrdenForm(ordenesColeccion){
+          var prototype = ordenesColeccion.data('prototype');
+
+          var index = ordenesColeccion.data('index');
+          var nuevaOrden = prototype.replace(/__name__/g, index);
+          ordenesColeccion.data('index', index + 1);
+
+          var nuevaOrdenTR = $(ordenesColeccion).append(nuevaOrden);
+    }
+
+    $('.delete-collection').click(function(){
+
+        deleteCollection($(this));
 
     });
+
+    $(document).on('click', '.delete-collection', function(){
+        deleteCollection($(this));
+
+    });
+
+    function deleteCollection(e){
+        event.preventDefault();
+        var son = e.parent();
+        var parent = son.parent();
+
+        parent.fadeOut(300, function(){
+            parent.remove();
+        });
+    }
 
 
 });
