5282a6d9e6429ae55e765d3bc4e2cb9f706f2a08
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-01-19 10:54:02 -0500

    SubmitActionsButtons

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -51,6 +51,7 @@ class UserController extends Controller
         $userForm = $this->createForm('artesanus_users_type', $user)->handleRequest($request);
 
         if ($userForm->isValid()) {
+            echo "<pre>";print_r($request);exit();
             $user->setPassword($this->get('artesanus.encoder')->encoder($user, $user->getPassword()));
             $usersManager->save($user);
             return $this->redirect($this->generateUrl('artesanus_console_acl_user', array('id' => $user->getId())));
