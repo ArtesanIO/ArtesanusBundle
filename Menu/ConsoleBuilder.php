cd2fec3605abe4540b4ec95940605f90f7fe56ab
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-01-27 12:58:45 -0500

    Modify template | Add sass

---
+++ Menu/ConsoleBuilder.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace ArtesanIO\ArtesanusBundle\Menu;
+
+use ArtesanIO\ArtesanusBundle\ArtesanusEvents;
+use ArtesanIO\ArtesanusBundle\Event\ArtesanusMenuEvent;
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class ConsoleBuilder extends ContainerAware
+{
+    public function consoleMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root', array('childrenAttributes' => array('class' => 'nav navbar-nav')));
+
+        $this->container->get('event_dispatcher')->dispatch(
+            ArtesanusEvents::ARTESANUS_NAVBAR,
+            new ArtesanusMenuEvent($factory, $menu)
+        );
+
+        return $menu;
+    }
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+    	$menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'nav navbar-nav');
+
+		$menu->addChild('Projects', array('route' => 'artesanus_console_acl_users'))
+			->setAttribute('icon', 'icon-list');
+
+		$menu->addChild('Employees', array('route' => 'artesanus_console_acl_users'))
+			->setAttribute('icon', 'icon-group');
+        return $menu;
+    }
+
+    public function userMenu(FactoryInterface $factory, array $options)
+    {
+    	$menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
+
+
+    	/*
+    	You probably want to show user specific information such as the username here. That's possible! Use any of the below methods to do this.
+
+    	if($this->container->get('security.context')->isGranted(array('ROLE_ADMIN', 'ROLE_USER'))) {} // Check if the visitor has any authenticated roles
+    	$username = $this->container->get('security.context')->getToken()->getUser()->getUsername(); // Get username of the current logged in user
+
+    	*/
+		$menu->addChild('User', array('label' => 'Hi visitor'))
+			->setAttribute('dropdown', true)
+			->setAttribute('icon', 'icon-user')
+            ->setAttribute('class', 'dropdown-toggle');
+
+		$menu['User']->addChild('Profile', array('route' => 'artesanus_console_acl_users'))
+			->setAttribute('icon', 'icon-edit');
+
+            $menu['User']->addChild('Logout', array('route' => 'artesanus_console_acl_users'))
+    			->setAttribute('icon', 'icon-edit');
+
+        return $menu;
+    }
+
+    public function aclMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'menu-section');
+
+		$menu->addChild('Users', array('route' => 'artesanus_console_acl_users'))
+			->setAttribute('icon', 'icon-list');
+
+		$menu->addChild('Groups', array('route' => 'artesanus_console_acl_groups'))
+			->setAttribute('icon', 'icon-group');
+
+            $menu->addChild('Roles', array('route' => 'artesanus_console_acl_roles'))
+    			->setAttribute('icon', 'icon-list');
+        return $menu;
+    }
+
+    public function usersNewMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'menu-plus');
+
+		$menu->addChild('+', array('route' => 'artesanus_console_acl_users_new'))
+			->setAttribute('icon', 'icon-list');
+        return $menu;
+    }
+
+    public function groupsNewMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'menu-plus');
+
+		$menu->addChild('+', array('route' => 'artesanus_console_acl_groups_new'))
+			->setAttribute('icon', 'icon-list');
+        return $menu;
+    }
+
+    public function rolesNewMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+    	$menu->setChildrenAttribute('class', 'menu-plus');
+
+		$menu->addChild('+', array('route' => 'artesanus_console_acl_roles_new'))
+			->setAttribute('icon', 'icon-list');
+        return $menu;
+    }
+}
+
+
+?>
