227516d33eb978e26275d8fa7f57852b39a100d1
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-02-19 17:02:47 -0500

    FOSUser

--- Entity/Users.php
+++ Entity/Users.php
@@ -3,124 +3,24 @@
 namespace ArtesanIO\ArtesanusBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use ArtesanIO\ArtesanusBundle\Model\UsersBase;
+use FOS\UserBundle\Entity\User as BaseUser;
+
 
 /**
- * @ORM\Table(name="artesanus_user")
+ * @ORM\Table(name="artesanus_users")
  * @ORM\Entity(repositoryClass="ArtesanIO\ArtesanusBundle\Entity\UsersRepository")
  */
-class Users extends UsersBase
+class Users extends BaseUser
 {
     /**
      * @ORM\Column(type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255, nullable=true)
-     */
-    private $name;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="Groups")
-     * @ORM\JoinColumn(name="group_id", referencedColumnName="id")
-     */
-
-    private $groups;
-
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
+    public function __construct()
     {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     * @return Users
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set groups
-     *
-     * @param \ArtesanIO\ArtesanusBundle\Entity\Groups $groups
-     * @return Users
-     */
-    public function setGroups(\ArtesanIO\ArtesanusBundle\Entity\Groups $groups = null)
-    {
-        $this->groups = $groups;
-
-        return $this;
-    }
-
-    /**
-     * Get groups
-     *
-     * @return \ArtesanIO\ArtesanusBundle\Entity\Groups
-     */
-    public function getGroups()
-    {
-        return $this->groups;
-    }
-
-    public function getRoles()
-    {
-
-        $roles = [];
-
-        foreach($this->getGroups()->getRoles() as $role)
-        {
-            $roles[$role->getRoles()->getRoleKey()] = $role->getRoles()->getRoleKey();
-        }
-
-        return $roles;
-    }
-
-    /**
-     * Set active
-     *
-     * @param boolean $active
-     * @return Users
-     */
-    public function setActive($active)
-    {
-        $this->active = $active;
-
-        return $this;
-    }
-
-    /**
-     * Get active
-     *
-     * @return boolean 
-     */
-    public function getActive()
-    {
-        return $this->active;
+        parent::__construct();
     }
 }
